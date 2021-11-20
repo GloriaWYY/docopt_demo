96e2964fc16fca39180ba43bf13c398a90300c2c
GloriaWYY <wanying.ye2020@gmail.com>
2021-11-19 16:29:18 -0800

    Complete main function for Q6.

--- demo_modify.R
+++ demo_modify.R
@@ -5,13 +5,18 @@
 Usage: demo.R <arg1> [<arg4>] --arg2=<arg2> [--arg3=<arg3>]
 Options:
 <arg>             Takes any value (this is a required positional argument)
-[<arg4>]            Takes any value (this is an optional positional argument)
+[<arg4>]          Takes any value (this is an optional positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 " -> doc
 
 library(docopt)
 opt <- docopt(doc)
-print(opt)
-print(typeof(opt))
-print(opt$arg4)
+
+main <- function(arg, arg4=NULL, arg2, arg3=NULL) {
+  print(opt)
+  print(typeof(opt))
+  print(opt$arg4)
+}
+
+main(opt$arg1, opt$arg4, opt$arg2, opt$arg3)
