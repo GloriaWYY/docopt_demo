73ae7820fd599cd708e03605a30e65e54e88c7d9
GloriaWYY <wanying.ye2020@gmail.com>
2021-11-19 17:29:18 -0800

    Fix typos.

--- demo_modify.R
+++ demo_modify.R
@@ -1,10 +1,11 @@
 # author: Tiffany Timbers
-# date: 2020-01-15
+# modified by: Wanying Ye
+# date: 2021-11-19
 
 "This script prints out docopt args.
 Usage: demo.R <arg1> [<arg4>] --arg2=<arg2> [--arg3=<arg3>]
 Options:
-<arg>             Takes any value (this is a required positional argument)
+<arg1>             Takes any value (this is a required positional argument)
 [<arg4>]          Takes any value (this is an optional positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
@@ -13,7 +14,7 @@ Options:
 library(docopt)
 opt <- docopt(doc)
 
-main <- function(arg, arg4=NULL, arg2, arg3=NULL) {
+main <- function(arg, arg4, arg2, arg3) {
   print(opt)
   print(typeof(opt))
   print(opt$arg4)
