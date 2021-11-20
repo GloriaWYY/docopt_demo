45ec2af49f2baadc524def4541d90419d1eb05fc
GloriaWYY <wanying.ye2020@gmail.com>
2021-11-19 17:15:50 -0800

    Modify demo.py according to Ex8.

---
+++ demo_modify.py
@@ -0,0 +1,19 @@
+# author: Tiffany Timbers
+# modified by: Wanying Ye
+# date: 2021-11-19
+
+"""This script prints out docopt args.
+Usage: demo.py <arg1> [<arg4>] --arg2=<arg2> [--arg3=<arg3>]
+
+Options:
+<arg1>             Takes any value (this is a required positional argument)
+[<arg4>]          Takes any value (this is an optional positional argument)
+--arg2=<arg2>     Takes any value (this is a required option)
+[--arg3=<arg3>]   Takes any value (this is an optional option)
+""" 
+
+from docopt import docopt
+opt = docopt(__doc__)
+print(opt)
+print(type(opt))
+print(opt["<arg4>"])
